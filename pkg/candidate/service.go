42180b0254fad4284dfbb142893b4e697eb6c9cb
Nimesh <nimesh.mittal@gmail.com>
2019-09-19 07:29:48 +0530

    Add code comments

--- pkg/candidate/service.go
+++ pkg/candidate/service.go
@@ -8,10 +8,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// ServiceContext maintains repo instance
 type ServiceContext struct {
 	DAO DBContext
 }
 
+// NewServiceContext creates new ServiceContext
 func NewServiceContext() *ServiceContext {
 
 	// candidateDBContext, err := daos.NewCandidateMongoDBContext(config.GetInstance().Database.MongoURL)
@@ -26,10 +28,12 @@ func NewServiceContext() *ServiceContext {
 	return &ServiceContext{DAO: *candidateDBContext}
 }
 
+// SafeClose gets called when main is about to complete
 func (ctx *ServiceContext) SafeClose() {
 	ctx.DAO.SafeClose()
 }
 
+// ListCandidates to list candidates with pagination
 func (ctx *ServiceContext) ListCandidates(fCtx *commons.FlowContext, limit int, offset int) (*[]Candidate, error) {
 	logrus.WithField(commons.TrackingID, fCtx.TrackingID).Info("listing candidates")
 
@@ -42,6 +46,7 @@ func (ctx *ServiceContext) ListCandidates(fCtx *commons.FlowContext, limit int, 
 	return candidates, nil
 }
 
+// GetCandidate gets candidate by candidate id
 func (ctx *ServiceContext) GetCandidate(fCtx *commons.FlowContext, cid string) (*Candidate, error) {
 	logrus.WithField(commons.TrackingID, fCtx.TrackingID).Info("get candidate by id")
 
@@ -53,6 +58,7 @@ func (ctx *ServiceContext) GetCandidate(fCtx *commons.FlowContext, cid string) (
 	return c, nil
 }
 
+// CreateCandidate creates candidate
 func (ctx *ServiceContext) CreateCandidate(f *commons.FlowContext, candidate *Candidate) (*Candidate, error) {
 	logrus.WithField(commons.TrackingID, f.TrackingID).Info("creating candidate")
 
@@ -64,6 +70,7 @@ func (ctx *ServiceContext) CreateCandidate(f *commons.FlowContext, candidate *Ca
 	return c, nil
 }
 
+// UpdateCandidate updates candidate by candidate id
 func (ctx *ServiceContext) UpdateCandidate(fCtx *commons.FlowContext, cid string, entity *Candidate) (string, error) {
 	logrus.WithField(commons.TrackingID, fCtx.TrackingID).Info("updating candidate")
 
@@ -76,6 +83,7 @@ func (ctx *ServiceContext) UpdateCandidate(fCtx *commons.FlowContext, cid string
 	return cid, nil
 }
 
+// DeleteCandidate deletes candidate by candidate id
 func (ctx *ServiceContext) DeleteCandidate(fCtx *commons.FlowContext, cid string) (*Candidate, error) {
 	logrus.WithField(commons.TrackingID, fCtx.TrackingID).Info("deleting candidate")
 
